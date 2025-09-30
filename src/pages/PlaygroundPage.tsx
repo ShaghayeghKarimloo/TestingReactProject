a98e1da4357932a48b9f6593ecd276b30282541f
ShaghayeghKarimloo <shaghayegh.karimloo@gmail.com>
2025-09-30 23:57:09 +0330

    Unit test in react

---
+++ src/pages/PlaygroundPage.tsx
@@ -0,0 +1,11 @@
+import ExpandableText from "../components/ExpandableText";
+import Onboarding from "../components/Onboarding";
+import TermsAndConditions from "../components/TermsAndConditions";
+
+
+
+const PlaygroundPage = () => {
+  return <ExpandableText text= "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque exercitationem reprehenderit nobis vel! Recusandae adipisci repudiandae perferendis! Amet omnis distinctio nobis consequuntur quis dolorum beatae numquam officia recusandae, laborum dignissimos reprehenderit harum iure odio tenetur, voluptatibus vel magni eveniet ipsum voluptates tempore laudantium. Qui excepturi neque beatae, vitae eos sequi nobis inventore accusantium temporibus ad ipsam magnam, autem soluta dolore eum nostrum distinctio id omnis unde expedita, labore at cupiditate repudiandae. Excepturi, ex magni praesentium vitae est repellat alias eaque fugit veritatis iste quos repellendus. Itaque odit, vel, amet dolorem, dolorum asperiores ullam tenetur consectetur recusandae voluptas non ipsum nemo?" />;
+};
+
+export default PlaygroundPage;
